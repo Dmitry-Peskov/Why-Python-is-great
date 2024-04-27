e71952603812279d1aec23f874f0fe825da94c46
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-04-27 21:34:05 +0300

    Поправил оппечатку. Добавил пояснениние к вызову функции

--- Декоратор/without_parameters.py
+++ Декоратор/without_parameters.py
@@ -8,7 +8,8 @@ def track_execution(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         original_result = func(*args, **kwargs)
-        msg = f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} вызов {func.__name__}() с args {args} и kawargs {kwargs} дал результат {original_result}"
+        msg = f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} вызов {func.__name__}()" \
+              f" с args {args} и kwargs {kwargs} дал результат {original_result}"
         print(msg)
         return original_result
     return wrapper
@@ -22,6 +23,7 @@ def test_func(a: int | float, b: int | float, c: int | float) -> int | float:
 
 if __name__ == "__main__":
     # делаем несколько вызовов нашей функции, что бы посмотреть как отрабатывает декоратор
+    # передаем значения и как позиционные аргументы и как именнованные
     result1 = test_func(a=10, b=4, c=15)
     result2 = test_func(3, 7, 11)
     result3 = test_func(3, 6, c=12)
