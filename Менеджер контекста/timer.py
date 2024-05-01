304c767a9978d0ad4aecfbefbcfb3e2d88f7885b
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-05-01 20:47:51 +0300

    Добавил комментариии

--- Менеджер контекста/timer.py
+++ Менеджер контекста/timer.py
@@ -9,11 +9,17 @@ class Timer:
     def __exit__(self, exc_type, exc_val, exc_tb):
         elapsed_time = time.time() - self.start_time
         print(f"С момента запуска таймера прошло {elapsed_time} секунд")
+        del self
 
 
 if __name__ == "__main__":
+
     with Timer() as timer:
+        # здесь мы получили экземпляр класса Timer
         time.sleep(3)
+    # здесь работа с таймером была завершена
 
     with Timer() as timer2:
+        # здесь мы получили новый экземпляр класса Timer
         time.sleep(1)
+    # здесь работа с таймером была завершена
